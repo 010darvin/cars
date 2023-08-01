875774665b7e2cc7bed664454567944f060ae80c
010darvin <133992859+010darvin@users.noreply.github.com>
2023-08-01 23:53:52 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -15,6 +15,58 @@ data_file 'CARCOLS_FILE' 'carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 data_file 'CONTENT_UNLOCKING_META_FILE' 'caraddoncontentunlocks.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'dlctext.meta'
+data_fire 'VEHICLE_VARIATTON_FILE  '2018 Audi e-tron GT
+data_fire 'VEHICLE_VERIATTON_FILE  '2018 Dodge Charger – PD
+data_fire 'VEHICLE_VERIATTON_FILE  '2017 Lamborghini Terzo Millennio
+data_fire 'VEHICLE_VERIATTON_FILE  '2013 Audi R8 V10 & PPI Razor Tuning
+data_fire 'VEHICLE_VERIATTON_FILE  'Audi R8 2020
+data_fire 'VEHICLE_VERIATTON_FILE  '2016 Audi RS6 C7 Performance
+data_fire 'VEHICLE_VERIATTON_FILE  '2010 Audi A8 4.2 FSI (D4)
+data_fire 'VEHICLE_VERIATTON_FILE  'Audi SQ7 2016 APT
+data_fire 'VEHICLE_VERIATTON_FILE  'Audi Q8 50TDI 2020
+data_fire 'VEHICLE_VERIATTON_FILE  'Audi RS5 2011
+data_fire 'VEHICLE_VERIATTON_FILE  'Audi RS3 Sportback 2018 APT
+data_fire 'VEHICLE_VERIATTON_FILE  '2020 Audi RS6 C8 Avant 
+data_fire 'VEHICLE_VERIATTON_FILE  'Audi A6 '20
+data_fire 'VEHICLE_VERIATTON_FILE  '2021 Audi RS6 Sedan (C8)
+data_fire 'VEHICLE_VERIATTON_FILE  'Audi RS5-R ABT
+data_fire 'VEHICLE_VERIATTON_FILE  'Mansory Audi RSQ8
+data_fire 'VEHICLE_VERIATTON_FILE  '2020 Audi RS7 Sportback (C8) 
+data_fire 'VEHICLE_VERIATTON_FILE  '2018 Audi A8
+data_fire 'VEHICLE_VERIATTON_FILE  '2016 Audi RS7 
+data_fire 'VEHICLE_VERIATTON_FILE  'Audi S8
+data_fire 'VEHICLE_VERIATTON_FILE  '2013 Audi TT RS 
+data_fire 'VEHICLE_VERIATTON_FILE  '2013 Audi RS4 Avant 
+data_fire 'VEHICLE_VERIATTON_FILE  '2016 Audi S8 plus
+data_fire 'VEHICLE_VERIATTON_FILE  '2023 Audi RS7 + ABT Version
+data_fire 'VEHICLE_VERIATTON_FILE  '2015 Audi A3 / S3 Sedan
+data_fire 'VEHICLE_VERIATTON_FILE  'Audi R8 Prior Edition 
+data_fire 'VEHICLE_VERIATTON_FILE  '2016 Mercedes-Benz AMG GT
+data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz AMG Vision GT
+data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz C63 Coupe AMG 
+data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz AMG GT63 2018
+data_fire 'VEHICLE_VERIATTON_FILE  '2019 G63 ///AMG
+data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz E63 AMG
+data_fire 'VEHICLE_VERIATTON_FILE  '2019 Mercedes Benz AMG G63 
+data_fire 'VEHICLE_VERIATTON_FILE  '2014 Mercedes-AMG S63 AMG
+data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz GL63 AMG
+data_fire 'VEHICLE_VERIATTON_FILE  '2020 Mercedes-Benz AMG GT Black Series 
+data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz Classe A45 AMG | Edition 
+data_fire 'VEHICLE_VERIATTON_FILE  '2018 Mercedes-Benz S65 AMG
+data_fire 'VEHICLE_VERIATTON_FILE  '2012 Mercedes-Benz C63 AMG Coupe Black Series
+data_fire 'VEHICLE_VERIATTON_FILE  '2021 Mercedes-Benz AMG S63 Coupe 4MATIC+
+data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz G63 AMG 6x6
+data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz E63 AMG
+data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes Benz S65 AMG W222 2014
+data_fire 'VEHICLE_VERIATTON_FILE  '
+data_fire 'VEHICLE_VERIATTON_FILE  '
+data_fire 'VEHICLE_VERIATTON_FILE  '
+data_fire 'VEHICLE_VERIATTON_FILE  '
+data_fire 'VEHICLE_VERIATTON_FILE  '
+data_fire 'VEHICLE_VERIATTON_FILE  '
+data_fire 'VEHICLE_VERIATTON_FILE  '
+data_fire 'VEHICLE_VERIATTON_FILE  '
+data_fire 'VEHICLE_VERIATTON_FILE  '
 
 
 client_script {
@@ -24,3 +76,4 @@ client_script {
 
 
 
+LSPD Mega Pack 4.1
