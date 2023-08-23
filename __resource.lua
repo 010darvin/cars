2d630397601c2363d023e0d2d5ebcaf14bc9c0de
010darvin <133992859+010darvin@users.noreply.github.com>
2023-08-23 18:29:42 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -104,9 +104,9 @@ data_fire 'VEHICLE_VERIATTON_FILE  'Explorer Yacht
 data_fire 'VEHICLE_VERIATTON_FILE  'Bateau Garde-Frontiere Suisse
 data_fire 'VEHICLE_VERIATTON_FILE  'USCG Legend-Class Cutter Bertholf
 data_fire 'VEHICLE_VERIATTON_FILE  'F-15 Silent Eagle
-data_fire 'VEHICLE_VERIATTON_FILE  '
-data_fire 'VEHICLE_VERIATTON_FILE  '
-data_fire 'VEHICLE_VERIATTON_FILE  '
+data_fire 'VEHICLE_VERIATTON_FILE  'McLaren 650S Coupé
+data_fire 'VEHICLE_VERIATTON_FILE  'GT-R R35 2017 liberti 
+data_fire 'VEHICLE_VERIATTON_FILE  '2022 Alpine A522
 data_fire 'VEHICLE_VERIATTON_FILE  '
 data_fire 'VEHICLE_VERIATTON_FILE  '
 data_fire 'VEHICLE_VERIATTON_FILE  '
