d04b0d4b9e9c37660af776ea17083fada66a125e
010darvin <133992859+010darvin@users.noreply.github.com>
2023-09-24 15:13:55 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -9,136 +9,136 @@ files {
 	'dlctext.meta',
 }
 
-data_file 'HANDLING_FILE' 'handling.meta'
-data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
-data_file 'CARCOLS_FILE' 'carcols.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
-data_file 'CONTENT_UNLOCKING_META_FILE' 'caraddoncontentunlocks.meta'
-data_file 'VEHICLE_VARIATION_FILE' 'dlctext.meta'
-data_fire 'VEHICLE_VARIATTON_FILE  '2018 Audi e-tron GT
-data_fire 'VEHICLE_VERIATTON_FILE  '2018 Dodge Charger – PD
-data_fire 'VEHICLE_VERIATTON_FILE  '2017 Lamborghini Terzo Millennio
-data_fire 'VEHICLE_VERIATTON_FILE  '2013 Audi R8 V10 & PPI Razor Tuning
-data_fire 'VEHICLE_VERIATTON_FILE  'Audi R8 2020
-data_fire 'VEHICLE_VERIATTON_FILE  '2016 Audi RS6 C7 Performance
-data_fire 'VEHICLE_VERIATTON_FILE  '2010 Audi A8 4.2 FSI (D4)
-data_fire 'VEHICLE_VERIATTON_FILE  'Audi SQ7 2016 APT
-data_fire 'VEHICLE_VERIATTON_FILE  'Audi Q8 50TDI 2020
-data_fire 'VEHICLE_VERIATTON_FILE  'Audi RS5 2011
-data_fire 'VEHICLE_VERIATTON_FILE  'Audi RS3 Sportback 2018 APT
-data_fire 'VEHICLE_VERIATTON_FILE  '2020 Audi RS6 C8 Avant 
-data_fire 'VEHICLE_VERIATTON_FILE  'Audi A6 '20
-data_fire 'VEHICLE_VERIATTON_FILE  '2021 Audi RS6 Sedan (C8)
-data_fire 'VEHICLE_VERIATTON_FILE  'Audi RS5-R ABT
-data_fire 'VEHICLE_VERIATTON_FILE  'Mansory Audi RSQ8
-data_fire 'VEHICLE_VERIATTON_FILE  '2020 Audi RS7 Sportback (C8) 
-data_fire 'VEHICLE_VERIATTON_FILE  '2018 Audi A8
-data_fire 'VEHICLE_VERIATTON_FILE  '2016 Audi RS7 
-data_fire 'VEHICLE_VERIATTON_FILE  'Audi S8
-data_fire 'VEHICLE_VERIATTON_FILE  '2013 Audi TT RS 
-data_fire 'VEHICLE_VERIATTON_FILE  '2013 Audi RS4 Avant 
-data_fire 'VEHICLE_VERIATTON_FILE  '2016 Audi S8 plus
-data_fire 'VEHICLE_VERIATTON_FILE  '2023 Audi RS7 + ABT Version
-data_fire 'VEHICLE_VERIATTON_FILE  '2015 Audi A3 / S3 Sedan
-data_fire 'VEHICLE_VERIATTON_FILE  'Audi R8 Prior Edition 
-data_fire 'VEHICLE_VERIATTON_FILE  '2016 Mercedes-Benz AMG GT
-data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz AMG Vision GT
-data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz C63 Coupe AMG 
-data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz AMG GT63 2018
-data_fire 'VEHICLE_VERIATTON_FILE  '2019 G63 ///AMG
-data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz E63 AMG
-data_fire 'VEHICLE_VERIATTON_FILE  '2019 Mercedes Benz AMG G63 
-data_fire 'VEHICLE_VERIATTON_FILE  '2014 Mercedes-AMG S63 AMG
-data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz GL63 AMG
-data_fire 'VEHICLE_VERIATTON_FILE  '2020 Mercedes-Benz AMG GT Black Series 
-data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz Classe A45 AMG | Edition 
-data_fire 'VEHICLE_VERIATTON_FILE  '2018 Mercedes-Benz S65 AMG
-data_fire 'VEHICLE_VERIATTON_FILE  '2012 Mercedes-Benz C63 AMG Coupe Black Series
-data_fire 'VEHICLE_VERIATTON_FILE  '2021 Mercedes-Benz AMG S63 Coupe 4MATIC+
-data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz G63 AMG 6x6
-data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz E63 AMG
-data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes Benz S65 AMG W222 2014
-data_fire 'VEHICLE_VERIATTON_FILE  '2021 BMW 5er 530D
-data_fire 'VEHICLE_VERIATTON_FILE  'BMW 745Le x Drive
-data_fire 'VEHICLE_VERIATTON_FILE  '2019 AMG GT63S
-data_fire 'VEHICLE_VERIATTON_FILE  '2020 Mercedes AMG GLE53 Coupé
-data_fire 'VEHICLE_VERIATTON_FILE  'Audi S5
-data_fire 'VEHICLE_VERIATTON_FILE  '2020 BMW Z4 M40i
-data_fire 'VEHICLE_VERIATTON_FILE  'Cupra Formenter
-data_fire 'VEHICLE_VERIATTON_FILE  'bmw M3 F80
-data_fire 'VEHICLE_VERIATTON_FILE  'bmw M5 E60 
-data_fire 'VEHICLE_VERIATTON_FILE  'bmw X6M F16 Breitbau
-data_fire 'VEHICLE_VERIATTON_FILE  'bmw M5 F90 2018 B
-data_fire 'VEHICLE_VERIATTON_FILE  '2019 BMW M8 
-data_fire 'VEHICLE_VERIATTON_FILE  '2012 BMW M5 f10 
-data_fire 'VEHICLE_VERIATTON_FILE  'bmw M5 F90
-data_fire 'VEHICLE_VERIATTON_FILE  'bmw M5 2021 
-data_fire 'VEHICLE_VERIATTON_FILE  'bmw M8 Competition MANSAUG
-data_fire 'VEHICLE_VERIATTON_FILE  'bmw X7 2021
-data_fire 'VEHICLE_VERIATTON_FILE  'bmw M3 E46 GTR
-data_fire 'VEHICLE_VERIATTON_FILE  'bmw X5M Competition 2020
-data_fire 'VEHICLE_VERIATTON_FILE  'porsche 911 GT3RS 4.0L
-data_fire 'VEHICLE_VERIATTON_FILE  '2020 Ferrari Roma
-data_fire 'VEHICLE_VERIATTON_FILE  'Lamborghini Urus
-data_fire 'VEHICLE_VERIATTON_FILE  '2017 Nissan GTR
-data_fire 'VEHICLE_VERIATTON_FILE  '2018 Audi A8 TFSI
-data_fire 'VEHICLE_VERIATTON_FILE  '2023 Lamborghini Revuelto
-data_fire 'VEHICLE_VERIATTON_FILE  '2011 Lamborghini Gallardo LP 570-4 Superleggera
-data_fire 'VEHICLE_VERIATTON_FILE  '1400HP Ford Mustang Hoonicorn
-data_fire 'VEHICLE_VERIATTON_FILE  '2022 Porsche 911 GT3
-data_fire 'VEHICLE_VERIATTON_FILE  'Toyota Supra a80
-data_fire 'VEHICLE_VERIATTON_FILE  'Ferrari Monza SP2 2019
-data_fire 'VEHICLE_VERIATTON_FILE  'Ferrari 488 Pista 2019
-data_fire 'VEHICLE_VERIATTON_FILE  'BMW M3/4 (S58B30) 
-data_fire 'VEHICLE_VERIATTON_FILE  'CBR 1000RR-R
-data_fire 'VEHICLE_VERIATTON_FILE  'Koenigsegg Agera RS
-data_fire 'VEHICLE_VERIATTON_FILE  '2023 Audi RS7 + ABT
-data_fire 'VEHICLE_VERIATTON_FILE  'Lamborghini Huracan Super Trofeo
-data_fire 'VEHICLE_VERIATTON_FILE  '2014 Rolls-Royce Wraith
-data_fire 'VEHICLE_VERIATTON_FILE  'Rolls Royce Cullinan Black
-data_fire 'VEHICLE_VERIATTON_FILE  '2014 rolls Royce Phantom
-data_fire 'VEHICLE_VERIATTON_FILE  'Rolls-Royce Cullinan Mansory 2021 
-data_fire 'VEHICLE_VERIATTON_FILE  'USS Forrestal
-data_fire 'VEHICLE_VERIATTON_FILE  'Urchin 2 Bulk Carrier
-data_fire 'VEHICLE_VERIATTON_FILE  'Explorer Yacht
-data_fire 'VEHICLE_VERIATTON_FILE  'Bateau Garde-Frontiere Suisse
-data_fire 'VEHICLE_VERIATTON_FILE  'USCG Legend-Class Cutter Bertholf
-data_fire 'VEHICLE_VERIATTON_FILE  'F-15 Silent Eagle
-data_fire 'VEHICLE_VERIATTON_FILE  'McLaren 650S Coupé
-data_fire 'VEHICLE_VERIATTON_FILE  'GT-R R35 2017 liberti 
-data_fire 'VEHICLE_VERIATTON_FILE  '2022 Alpine A522
-data_fire 'VEHICLE_VERIATTON_FILE  'ZL1 Camaro Unmarked  PD
-data_fire 'VEHICLE_VERIATTON_FILE  'E63 Unmarked PD
-data_fire 'VEHICLE_VERIATTON_FILE  '2015 Subaru WRX STI PD
-data_fire 'VEHICLE_VERIATTON_FILE  '2021 Ford F150 Limited Unmarked PD
-data_fire 'VEHICLE_VERIATTON_FILE  '2020 Ford Explorer Unmarked PD 
-data_fire 'VEHICLE_VERIATTON_FILE  '2021 Chevy Tahoe Unmarked PD 
-data_fire 'VEHICLE_VERIATTON_FILE  '2021 Toyota RAV4 Unmarked PD
-data_fire 'VEHICLE_VERIATTON_FILE  '2012 Ford Expedition Unmarked PD
-data_fire 'VEHICLE_VERIATTON_FILE  '2016 Dodge Ram Unmarked PD
-data_fire 'VEHICLE_VERIATTON_FILE  '2011 Crown Victoria Unmarked PD
-data_fire 'VEHICLE_VERIATTON_FILE  '2018 hlcagher 
-data_fire 'VEHICLE_VERIATTON_FILE  '2016 hlxploer 
-data_fire 'VEHICLE_VERIATTON_FILE  '2016 hltauros 
-data_fire 'VEHICLE_VERIATTON_FILE  '2023 Huracan Tecnica 
-data_fire 'VEHICLE_VERIATTON_FILE  '2021 Audi RS6-R ABT
-data_fire 'VEHICLE_VERIATTON_FILE  '2016 BMW M4 G82 M-Performance
-data_fire 'VEHICLE_VERIATTON_FILE  '2010 Ferrari 458 Italia 
-data_fire 'VEHICLE_VERIATTON_FILE  'BMW M4 CS 2018
-data_fire 'VEHICLE_VERIATTON_FILE  '2018 bmw m5 f90 
-data_fire 'VEHICLE_VERIATTON_FILE  '2018 bmw_pd 
-data_fire 'VEHICLE_VERIATTON_FILE  '2016 xploer unmarkt 
-data_fire 'VEHICLE_VERIATTON_FILE  '"2020 Ferrari SF90 Stradale
-data_fire 'VEHICLE_VERIATTON_FILE  '2019 Mercedes-Benz S650 Maybach 
-data_fire 'VEHICLE_VERIATTON_FILE  '"Ferrari Enzo & Gemballa MIG-U1
-data_fire 'VEHICLE_VERIATTON_FILE  '"2019 Mercedes-Benz S650 Pullman Maybach
-data_fire 'VEHICLE_VERIATTON_FILE  '"2015 bmw s1000 rr
-data_fire 'VEHICLE_VERIATTON_FILE  '"2015 honday r7 
-data_fire 'VEHICLE_VERIATTON_FILE  '"2018 BMW X5 
-#data_fire 'VEHICLE_VERIATTON_FILE '"2018 BMW X6M 
-data_fire 'VEHICLE_VERIATTON_FILE  '"2018 BMW M8 CS
-data_fire 'VEHICLE_VERIATTON_FILE  '"2018 BMW M5 Combi
-data_fire 'VEHICLE_VERIATTON_FILE  '"2018 BMW M5 Competition
+start data_file 'HANDLING_FILE' 'handling.meta'
+start data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+start data_file 'CARCOLS_FILE' 'carcols.meta'
+start data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+start data_file 'CONTENT_UNLOCKING_META_FILE' 'caraddoncontentunlocks.meta'
+start data_file 'VEHICLE_VARIATION_FILE' 'dlctext.meta'
+start data_fire 'VEHICLE_VARIATTON_FILE  '2018 Audi e-tron GT
+start data_fire 'VEHICLE_VERIATTON_FILE  '2018 Dodge Charger – PD
+start data_fire 'VEHICLE_VERIATTON_FILE  '2017 Lamborghini Terzo Millennio
+start data_fire 'VEHICLE_VERIATTON_FILE  '2013 Audi R8 V10 & PPI Razor Tuning
+start data_fire 'VEHICLE_VERIATTON_FILE  'Audi R8 2020
+start data_fire 'VEHICLE_VERIATTON_FILE  '2016 Audi RS6 C7 Performance
+start data_fire 'VEHICLE_VERIATTON_FILE  '2010 Audi A8 4.2 FSI (D4)
+start data_fire 'VEHICLE_VERIATTON_FILE  'Audi SQ7 2016 APT
+start data_fire 'VEHICLE_VERIATTON_FILE  'Audi Q8 50TDI 2020
+start data_fire 'VEHICLE_VERIATTON_FILE  'Audi RS5 2011
+start data_fire 'VEHICLE_VERIATTON_FILE  'Audi RS3 Sportback 2018 APT
+start data_fire 'VEHICLE_VERIATTON_FILE  '2020 Audi RS6 C8 Avant 
+start data_fire 'VEHICLE_VERIATTON_FILE  'Audi A6 '20
+start data_fire 'VEHICLE_VERIATTON_FILE  '2021 Audi RS6 Sedan (C8)
+start data_fire 'VEHICLE_VERIATTON_FILE  'Audi RS5-R ABT
+start data_fire 'VEHICLE_VERIATTON_FILE  'Mansory Audi RSQ8
+start data_fire 'VEHICLE_VERIATTON_FILE  '2020 Audi RS7 Sportback (C8) 
+start data_fire 'VEHICLE_VERIATTON_FILE  '2018 Audi A8
+start data_fire 'VEHICLE_VERIATTON_FILE  '2016 Audi RS7 
+start data_fire 'VEHICLE_VERIATTON_FILE  'Audi S8
+start data_fire 'VEHICLE_VERIATTON_FILE  '2013 Audi TT RS 
+start data_fire 'VEHICLE_VERIATTON_FILE  '2013 Audi RS4 Avant 
+start data_fire 'VEHICLE_VERIATTON_FILE  '2016 Audi S8 plus
+start data_fire 'VEHICLE_VERIATTON_FILE  '2023 Audi RS7 + ABT Version
+start data_fire 'VEHICLE_VERIATTON_FILE  '2015 Audi A3 / S3 Sedan
+start data_fire 'VEHICLE_VERIATTON_FILE  'Audi R8 Prior Edition 
+start data_fire 'VEHICLE_VERIATTON_FILE  '2016 Mercedes-Benz AMG GT
+start data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz AMG Vision GT
+start data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz C63 Coupe AMG 
+start data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz AMG GT63 2018
+start data_fire 'VEHICLE_VERIATTON_FILE  '2019 G63 ///AMG
+start data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz E63 AMG
+start data_fire 'VEHICLE_VERIATTON_FILE  '2019 Mercedes Benz AMG G63 
+start data_fire 'VEHICLE_VERIATTON_FILE  '2014 Mercedes-AMG S63 AMG
+start data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz GL63 AMG
+start data_fire 'VEHICLE_VERIATTON_FILE  '2020 Mercedes-Benz AMG GT Black Series 
+start data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz Classe A45 AMG | Edition 
+start data_fire 'VEHICLE_VERIATTON_FILE  '2018 Mercedes-Benz S65 AMG
+start data_fire 'VEHICLE_VERIATTON_FILE  '2012 Mercedes-Benz C63 AMG Coupe Black Series
+start data_fire 'VEHICLE_VERIATTON_FILE  '2021 Mercedes-Benz AMG S63 Coupe 4MATIC+
+start data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz G63 AMG 6x6
+start data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes-Benz E63 AMG
+start data_fire 'VEHICLE_VERIATTON_FILE  'Mercedes Benz S65 AMG W222 2014
+start data_fire 'VEHICLE_VERIATTON_FILE  '2021 BMW 5er 530D
+start data_fire 'VEHICLE_VERIATTON_FILE  'BMW 745Le x Drive
+start data_fire 'VEHICLE_VERIATTON_FILE  '2019 AMG GT63S
+start data_fire 'VEHICLE_VERIATTON_FILE  '2020 Mercedes AMG GLE53 Coupé
+start data_fire 'VEHICLE_VERIATTON_FILE  'Audi S5
+start data_fire 'VEHICLE_VERIATTON_FILE  '2020 BMW Z4 M40i
+start data_fire 'VEHICLE_VERIATTON_FILE  'Cupra Formenter
+start data_fire 'VEHICLE_VERIATTON_FILE  'bmw M3 F80
+start data_fire 'VEHICLE_VERIATTON_FILE  'bmw M5 E60 
+start data_fire 'VEHICLE_VERIATTON_FILE  'bmw X6M F16 Breitbau
+start data_fire 'VEHICLE_VERIATTON_FILE  'bmw M5 F90 2018 B
+start data_fire 'VEHICLE_VERIATTON_FILE  '2019 BMW M8 
+start data_fire 'VEHICLE_VERIATTON_FILE  '2012 BMW M5 f10 
+start data_fire 'VEHICLE_VERIATTON_FILE  'bmw M5 F90
+start data_fire 'VEHICLE_VERIATTON_FILE  'bmw M5 2021 
+start data_fire 'VEHICLE_VERIATTON_FILE  'bmw M8 Competition MANSAUG
+start data_fire 'VEHICLE_VERIATTON_FILE  'bmw X7 2021
+start data_fire 'VEHICLE_VERIATTON_FILE  'bmw M3 E46 GTR
+start data_fire 'VEHICLE_VERIATTON_FILE  'bmw X5M Competition 2020
+start data_fire 'VEHICLE_VERIATTON_FILE  'porsche 911 GT3RS 4.0L
+start data_fire 'VEHICLE_VERIATTON_FILE  '2020 Ferrari Roma
+start data_fire 'VEHICLE_VERIATTON_FILE  'Lamborghini Urus
+start data_fire 'VEHICLE_VERIATTON_FILE  '2017 Nissan GTR
+start data_fire 'VEHICLE_VERIATTON_FILE  '2018 Audi A8 TFSI
+start data_fire 'VEHICLE_VERIATTON_FILE  '2023 Lamborghini Revuelto
+start data_fire 'VEHICLE_VERIATTON_FILE  '2011 Lamborghini Gallardo LP 570-4 Superleggera
+start data_fire 'VEHICLE_VERIATTON_FILE  '1400HP Ford Mustang Hoonicorn
+start data_fire 'VEHICLE_VERIATTON_FILE  '2022 Porsche 911 GT3
+start data_fire 'VEHICLE_VERIATTON_FILE  'Toyota Supra a80
+start data_fire 'VEHICLE_VERIATTON_FILE  'Ferrari Monza SP2 2019
+start data_fire 'VEHICLE_VERIATTON_FILE  'Ferrari 488 Pista 2019
+start data_fire 'VEHICLE_VERIATTON_FILE  'BMW M3/4 (S58B30) 
+start data_fire 'VEHICLE_VERIATTON_FILE  'CBR 1000RR-R
+start data_fire 'VEHICLE_VERIATTON_FILE  'Koenigsegg Agera RS
+start data_fire 'VEHICLE_VERIATTON_FILE  '2023 Audi RS7 + ABT
+start data_fire 'VEHICLE_VERIATTON_FILE  'Lamborghini Huracan Super Trofeo
+start data_fire 'VEHICLE_VERIATTON_FILE  '2014 Rolls-Royce Wraith
+start data_fire 'VEHICLE_VERIATTON_FILE  'Rolls Royce Cullinan Black
+start data_fire 'VEHICLE_VERIATTON_FILE  '2014 rolls Royce Phantom
+start data_fire 'VEHICLE_VERIATTON_FILE  'Rolls-Royce Cullinan Mansory 2021 
+start data_fire 'VEHICLE_VERIATTON_FILE  'USS Forrestal
+start data_fire 'VEHICLE_VERIATTON_FILE  'Urchin 2 Bulk Carrier
+start data_fire 'VEHICLE_VERIATTON_FILE  'Explorer Yacht
+start data_fire 'VEHICLE_VERIATTON_FILE  'Bateau Garde-Frontiere Suisse
+start data_fire 'VEHICLE_VERIATTON_FILE  'USCG Legend-Class Cutter Bertholf
+start data_fire 'VEHICLE_VERIATTON_FILE  'F-15 Silent Eagle
+start data_fire 'VEHICLE_VERIATTON_FILE  'McLaren 650S Coupé
+start data_fire 'VEHICLE_VERIATTON_FILE  'GT-R R35 2017 liberti 
+start data_fire 'VEHICLE_VERIATTON_FILE  '2022 Alpine A522
+start data_fire 'VEHICLE_VERIATTON_FILE  'ZL1 Camaro Unmarked  PD
+start data_fire 'VEHICLE_VERIATTON_FILE  'E63 Unmarked PD
+start data_fire 'VEHICLE_VERIATTON_FILE  '2015 Subaru WRX STI PD
+start data_fire 'VEHICLE_VERIATTON_FILE  '2021 Ford F150 Limited Unmarked PD
+start data_fire 'VEHICLE_VERIATTON_FILE  '2020 Ford Explorer Unmarked PD 
+start data_fire 'VEHICLE_VERIATTON_FILE  '2021 Chevy Tahoe Unmarked PD 
+start data_fire 'VEHICLE_VERIATTON_FILE  '2021 Toyota RAV4 Unmarked PD
+start data_fire 'VEHICLE_VERIATTON_FILE  '2012 Ford Expedition Unmarked PD
+start data_fire 'VEHICLE_VERIATTON_FILE  '2016 Dodge Ram Unmarked PD
+start data_fire 'VEHICLE_VERIATTON_FILE  '2011 Crown Victoria Unmarked PD
+start data_fire 'VEHICLE_VERIATTON_FILE  '2018 hlcagher 
+start data_fire 'VEHICLE_VERIATTON_FILE  '2016 hlxploer 
+start data_fire 'VEHICLE_VERIATTON_FILE  '2016 hltauros 
+start data_fire 'VEHICLE_VERIATTON_FILE  '2023 Huracan Tecnica 
+start data_fire 'VEHICLE_VERIATTON_FILE  '2021 Audi RS6-R ABT
+start data_fire 'VEHICLE_VERIATTON_FILE  '2016 BMW M4 G82 M-Performance
+start data_fire 'VEHICLE_VERIATTON_FILE  '2010 Ferrari 458 Italia 
+start data_fire 'VEHICLE_VERIATTON_FILE  'BMW M4 CS 2018
+start data_fire 'VEHICLE_VERIATTON_FILE  '2018 bmw m5 f90 
+start data_fire 'VEHICLE_VERIATTON_FILE  '2018 bmw_pd 
+start data_fire 'VEHICLE_VERIATTON_FILE  '2016 xploer unmarkt 
+start data_fire 'VEHICLE_VERIATTON_FILE  '"2020 Ferrari SF90 Stradale
+start data_fire 'VEHICLE_VERIATTON_FILE  '2019 Mercedes-Benz S650 Maybach 
+start data_fire 'VEHICLE_VERIATTON_FILE  '"Ferrari Enzo & Gemballa MIG-U1
+start data_fire 'VEHICLE_VERIATTON_FILE  '"2019 Mercedes-Benz S650 Pullman Maybach
+start data_fire 'VEHICLE_VERIATTON_FILE  '"2015 bmw s1000 rr
+start data_fire 'VEHICLE_VERIATTON_FILE  '"2015 honday r7 
+start data_fire 'VEHICLE_VERIATTON_FILE  '"2018 BMW X5 
+start data_fire 'VEHICLE_VERIATTON_FILE '"2018 BMW X6M 
+start data_fire 'VEHICLE_VERIATTON_FILE  '"2018 BMW M8 CS
+start data_fire 'VEHICLE_VERIATTON_FILE  '"2018 BMW M5 Combi
+start data_fire 'VEHICLE_VERIATTON_FILE  '"2018 BMW M5 Competition
 
 client_script {
     'vehicle_names.lua'
